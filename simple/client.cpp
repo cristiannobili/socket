67955e233c55e1c1f29a03c51dc2e9e2120612f1
Cristian Nobili <c.nobili@gmail.com>
2021-11-10 17:40:53 +0100

    fix

--- simple/client.cpp
+++ simple/client.cpp
@@ -1,11 +1,4 @@
-#ifdef _WIN32
 #include <winsock.h>
-#else
-#define closesocket close
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#endif
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,13 +7,11 @@
 #define BUFFERSIZE 512
 #define PORT 8080
 #define IPADDRESS "127.0.0.1"
-#pragma comment(lib, "ws2_32.lib") //Winsock Library
+#pragma comment(lib, "ws2_32.lib") 
 
 void ClearWinSock()
 {
-#ifdef _WIN32
    WSACleanup();
-#endif
 }
 
 int manageError(char *errorMessage, int server_fd)
@@ -33,7 +24,6 @@ int manageError(char *errorMessage, int server_fd)
 
 int main(int argc, char const *argv[])
 {
-#ifdef _WIN32
    WSADATA wsaData;
    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
    if (iResult != 0)
@@ -41,9 +31,6 @@ int main(int argc, char const *argv[])
       printf("error at WSASturtup\n");
       return 0;
    }
-#endif
-
-   // Socket creation
    int client_fd, valread;
    char message[50];
 
@@ -53,21 +40,16 @@ int main(int argc, char const *argv[])
    {
       return manageError("socket creation failed.\n", client_fd);
    }
-   // Server address construction
    struct sockaddr_in address;
    memset(&address, 0, sizeof(address));
    address.sin_family = AF_INET;
-   address.sin_addr.s_addr = inet_addr(IPADDRESS); // server IP
-   address.sin_port = htons(PORT);                 // Server port
-
-   // Connection to the server
+   address.sin_addr.s_addr = inet_addr(IPADDRESS); 
+   address.sin_port = htons(PORT);
    if (connect(client_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
    {
       return manageError("Failed to connect.\n", client_fd);
    }
-
    printf("Client connected to server.\n");
-   
    while (message != "close")
    {
       memset(message, 0, sizeof(message));
@@ -75,15 +57,9 @@ int main(int argc, char const *argv[])
       std::cin.getline(message, 50);
       send(client_fd, message, strlen(message), 0);
       printf("Hello message sent\n");
-#ifdef _WIN32
       valread = recv(client_fd, buffer, 1024, 0);
-#else
-      valread = read(client_fd, buffer, 1024);
-#endif
-
       printf("%s\n", buffer);
    }
-
    closesocket(client_fd);
    ClearWinSock();
    printf("\n");
--- simple/server.cpp
+++ simple/server.cpp
@@ -1,11 +1,4 @@
-#ifdef _WIN32
 #include <winsock.h>
-#else
-#define closesocket close
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#endif
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,13 +6,11 @@
 #define BUFFERSIZE 512
 #define PORT 8080
 #define IPADDRESS "127.0.0.1"
-#pragma comment(lib, "ws2_32.lib") //Winsock Library
+#pragma comment(lib, "ws2_32.lib")
 
 void ClearWinSock()
 {
-#ifdef _WIN32
    WSACleanup();
-#endif
 }
 
 int manageError(char *errorMessage, int server_fd)
@@ -32,7 +23,6 @@ int manageError(char *errorMessage, int server_fd)
 
 int main(void)
 {
-#ifdef _WIN32
    WSADATA wsaData;
    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
    if (iResult != 0)
@@ -40,9 +30,6 @@ int main(void)
       printf("error at WSASturtup\n");
       return 0;
    }
-#endif
-
-   // Socket creation
    int server_fd, new_socket, valread;
    char buffer[1024] = {0};
    char *hello = "Received from serr";
@@ -51,59 +38,36 @@ int main(void)
    {
       return manageError("socket creation failed.\n", server_fd);
    }
-
-   // Server address construction
    struct sockaddr_in address;
    int address_len = sizeof(address);
    memset(&address, 0, address_len);
    address.sin_family = AF_INET;
-   address.sin_addr.s_addr = inet_addr(IPADDRESS); // server IP
-   address.sin_port = htons(PORT);                 // Server port
-
-
-   // Create connection
+   address.sin_addr.s_addr = inet_addr(IPADDRESS);
+   address.sin_port = htons(PORT);
    if (bind(server_fd, (struct sockaddr *)&address,
             sizeof(address)) < 0)
    {
       return manageError("Failed to bind.\n", server_fd);
    }
-
    if (listen(server_fd, 3) < 0)
    {
       return manageError("Failed to listen.\n", server_fd);
    }
-
-#ifdef _WIN32
-      if ((new_socket = accept(server_fd , (struct sockaddr *)&address, &address_len)) < 0)
-      {
-         return manageError("Failed to Accept.\n", server_fd);
-      }
-#else
-      if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                               (socklen_t *)&address_len)) < 0)
-      {
-         return manageError("Failed to Accept.\n", server_fd);
-      }
-#endif
-
+   if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &address_len)) < 0)
+   {
+      return manageError("Failed to Accept.\n", server_fd);
+   }
    while (buffer != "close")
    {
       memset(buffer, 0, sizeof(buffer));
-#ifdef _WIN32
-      valread = recv(new_socket, buffer, 1024, 0);
-#else
-      valread = read(new_socket, buffer, 1024);
-#endif
-
+      valread = recv(new_socket, buffer, BUFFERSIZE, 0);
       printf("%s\n", buffer);
       send(new_socket, hello, strlen(hello), 0);
       printf("Hello message sent\n");
    }
-
    closesocket(server_fd);
    ClearWinSock();
    printf("\n");
    system("pause");
    return (0);
-
 }
